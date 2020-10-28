9ee35820165ab91fa9b9a90a7201cd1ac0397228
justKolya <42074576+justKolya@users.noreply.github.com>
2020-10-28 23:15:32 +0200

    Update HRP.py

--- HRP.py
+++ HRP.py
@@ -6,6 +6,9 @@ import os
 import tensorflow as tf
 import cv2
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from toGmail import *
 from utils import label_map_util
@@ -17,8 +20,6 @@ model = 'human_recognition_graph'
 graphPath = model + '/frozen_inference_graph.pb'
 labelPath = os.path.join('training', 'object-detection.pbtxt')
 
-numberOfClasses = 1
-
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -29,13 +30,38 @@ with detection_graph.as_default():
 
 label_map = label_map_util.load_labelmap(labelPath)
 categories = label_map_util.convert_label_map_to_categories(
-    label_map, max_num_classes=numberOfClasses, use_display_name=True)
+    label_map, max_num_classes=1, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 def load_image_into_numpy_array(image):
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
+
+fromEmail = '************'
+
+fromEmailPassword = '*************'
+
+toEmail = '**************'
+
+def sendEmail():
+
+	print("SmartSecurityCamera found person!")
+	msgRoot = MIMEMultipart('related')
+	msgRoot['Subject'] = 'Оповіщення SmartSecurityCamera'
+	msgRoot['From'] = fromEmail
+	msgRoot['To'] = toEmail
+
+	msgAlternative = MIMEMultipart('alternative')
+	msgRoot.attach(msgAlternative)
+	msgText = MIMEText('На Вашій території знаходиться постороння людина!')
+	msgAlternative.attach(msgText)
+
+	smtp = smtplib.SMTP('smtp.gmail.com', 587)
+	smtp.starttls()
+	smtp.login(fromEmail, fromEmailPassword)
+	smtp.sendmail(fromEmail, toEmail, msgRoot.as_string())
+	smtp.quit()
 
 with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
@@ -52,6 +78,11 @@ with detection_graph.as_default():
             (boxes, scores, classes, num_detections) = sess.run(
                 [boxes, scores, classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
+           
+            if abs(np.mean(scores)) > 0.01:
+            	sendEmail()
+
+            #print(abs(np.mean(scores)))
 
             vis_util.visualize_boxes_and_labels_on_image_array(
                 image_np,
@@ -62,12 +93,8 @@ with detection_graph.as_default():
                 use_normalized_coordinates=True,
                 line_thickness=8)
 
-            cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
+            cv2.imshow('HRP', cv2.resize(image_np, (800, 600)))
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
-
-            if abs(np.mean(scores)) > 0.01:
-                print("SmartSecurityCamera found person!")
-                sendEmail()
